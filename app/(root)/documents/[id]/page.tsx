88004b6d3b290644e2c59cc407ad7b9b49c1564f
SureshKuchana <kuchana123.suresh@gmail.com>
2024-08-04 16:38:40 +0530

    initial setup

---
+++ app/(root)/documents/[id]/page.tsx
@@ -0,0 +1,13 @@
+import { Editor } from "@/components/editor/Editor";
+import React from "react";
+
+const Document = () => {
+  return (
+    <>
+      <div>Document</div>
+      <Editor />
+    </>
+  );
+};
+
+export default Document;
